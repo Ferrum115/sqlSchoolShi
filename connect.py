4971eb4e10837be12fd435012796a81e4c78826f
Ferrum115 <neomozg15@gmail.com>
2025-03-11 18:46:44 +0500

    added server connection file

---
+++ connect.py
@@ -0,0 +1,13 @@
+import psycopg2
+
+hst = '79.174.88.238'
+prt = 15221
+name = 'school_db'
+usr = 'school'
+pswd = 'School1234*'
+try:
+    conn = psycopg2.connect(database=name, user=usr, password=pswd, host=hst, port=prt)
+except:
+    print('bro ur database does weird shit')
+finally:
+    print(conn)
