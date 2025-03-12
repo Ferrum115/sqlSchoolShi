34bf9abaf9a45921b5876f04a64fb61f01442c0f
Ferrum115 <neomozg15@gmail.com>
2025-03-12 08:24:43 +0500

    changed migrate contents

--- migrate.sql
+++ migrate.sql
@@ -1,5 +1,16 @@
 create schema if not exists vasyutinsky_ryabov;
 
-create table IF NOT EXISTS vasyutinsky_ryabov.users (
-    
+create table IF NOT EXISTS vasyutinsky_ryabov.car (
+  carID INT PRIMARY KEY,
+  age INT NOT NULL,
+  model TEXT NOT NULL,
+  color TEXT NOT NULL,
+  carType TEXT NOT NULL,
+  accidentID INT[]
+)
+
+create table IF NOT EXISTS vasyutinsky_ryabov.accident (
+  ID INT PRIMARY KEY,
+  damaged TEXT[] NOT NULL,
+  accidentDate DATE NOT NULL
 )
