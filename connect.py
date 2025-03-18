8d02adfaab0c06bb74e0c8302e3e269cfbfcf186
Ferrum115 <neomozg15@gmail.com>
2025-03-18 19:07:58 +0500

    save|load functions implemented

--- connect.py
+++ connect.py
@@ -1,15 +1,42 @@
 import psycopg2
 
+#функции
+
+def load(carId):
+    cur.execute(f'SELECT carID={carId} from vr.cars;')
+    return cur.fetchone()
+
+def save(carId, carAge, carModel, carColor, carType, accidentId):
+    cur.execute(f'INSERT INTO vr.cars (carID, age, model, color, carType, accidentID) VALUES ({carId}, {carAge}, {carModel}, {carColor}, {carType}, {accidentId})')
+    print('car object saved')
+
+#данные ДБшки
 hst = '79.174.88.238'
 prt = 15221
 name = 'school_db'
 usr = 'school'
 pswd = 'School1234*'
-try:
-    conn = psycopg2.connect(database=name, user=usr, password=pswd, host=hst, port=prt)
-    with conn as cursor:
-        cursor.execute(open("migrate.sql", "r").read())
-except:
-    print('bro ur database does weird shit')
-finally:
-    print(conn)
+
+#соединяемся
+conn = psycopg2.connect(database=name, user=usr, password=pswd, host=hst, port=prt)
+cur = conn.cursor()
+mg_command = ''
+with open("migrate.sql") as f: #мигрируем
+    mg_command = f.read()
+
+with conn.cursor() as curse:
+    for s in mg_command.split(";"):
+        if s.strip():
+            curse.execute(s.strip())
+
+#примеры функций
+save(01, 10, 'porche 911', 'blue', 'race', [])
+
+load(01)
+
+#сохраняем изменения
+conn.commit()
+
+#закрываем соединение            
+cur.close()
+conn.close()
--- migrate.sql
+++ migrate.sql
@@ -1,5 +1,16 @@
 create schema if not exists vasyutinsky_ryabov;
 
-create table IF NOT EXISTS vasyutinsky_ryabov.users (
-    
-)
+CREATE TABLE IF NOT EXISTS vr.cars (
+  carID INT PRIMARY KEY,
+  age INT NOT NULL,
+  model TEXT NOT NULL,
+  color TEXT NOT NULL,
+  carType TEXT NOT NULL,
+  accidentID INT[]
+);
+
+CREATE TABLE IF NOT EXISTS vr.accident (
+  ID INT PRIMARY KEY,
+  damaged TEXT[] NOT NULL,
+  accidentDate DATE NOT NULL
+);
