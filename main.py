6a392e1fb9d0c7008a6d7e0dcc2d93670d5019d5
Ferrum115 <neomozg15@gmail.com>
2025-04-20 16:22:43 +0500

    fixed DB name

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import uvicorn
 import datetime
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-#функции
+
 
 app = FastAPI()
 
@@ -33,37 +33,37 @@ async def root():
 
 @app.get('/get/cars/byID/')
 async def getCar(id: int):
-    cur.execute(f'SELECT * FROM vr.cars WHERE carID = {id};')
+    cur.execute(f'SELECT * FROM vasyutinsky_ryabov.cars WHERE carID = {id};')
     r = cur.fetchall()
     return JSONResponse(r)
 
 @app.get('/get/cars/byType/')
 async def getCarByType(cType: str):
-    cur.execute(f'SELECT * FROM vr.cars WHERE carType = {cType};')
+    cur.execute(f'SELECT * FROM vasyutinsky_ryabov.cars WHERE carType = {cType};')
     r = cur.fetchall()
     return JSONResponse(r)
 
 @app.get('/get/cars/byColor/')
 async def getCarByColor(c: str):
-    cur.execute(f'SELECT * FROM vr.cars WHERE color = {c};')
+    cur.execute(f'SELECT * FROM vasyutinsky_ryabov.cars WHERE color = {c};')
     r = cur.fetchall()
     return JSONResponse(r)
 
 @app.get('/get/cars/byModel/')
 async def getCarByModel(mdl: str):
-    cur.execute(f'SELECT * FROM vr.cars WHERE model = {mdl};')
+    cur.execute(f'SELECT * FROM vasyutinsky_ryabov.cars WHERE model = {mdl};')
     r = cur.fetchall()
     return JSONResponse(r)
 
 @app.get('/get/cars/byAge/')
 async def getCarByAge(minimum: int, maximum: int):
-    cur.execute(f'SELECT * FROM vr.cars WHERE age BETWEEN {minimum} AND {maximum};')
+    cur.execute(f'SELECT * FROM vasyutinsky_ryabov.cars WHERE age BETWEEN {minimum} AND {maximum};')
     r = cur.fetchall()
     return JSONResponse(r)
 
 @app.get('/get/accidents/')
 async def getAccidents():
-    cur.execute(f'SELECT damaged, carid FROM vr.accident INNER JOIN vr.a2c ON accidentid = ID ORDER BY vr.accident.accidentDate ASC;')
+    cur.execute(f'SELECT damaged, carid FROM vasyutinsky_ryabov.accident INNER JOIN vasyutinsky_ryabov.a2c ON accidentid = ID ORDER BY vasyutinsky_ryabov.accident.accidentDate ASC;')
     r = cur.fetchall()
     return JSONResponse(r)
 
@@ -71,14 +71,14 @@ async def getAccidents():
 
 @app.post('/add/car/')
 async def addCar(id: int, ag: int, mdl: str, clr: str, typ: str):
-    cur.execute(f'INSERT INTO vr.cars (carID, age, model, color, carType) VALUES ({id}, {ag}, {mdl}, {clr}, {typ});')
+    cur.execute(f'INSERT INTO vasyutinsky_ryabov.cars (carID, age, model, color, carType) VALUES ({id}, {ag}, {mdl}, {clr}, {typ});')
     conn.commit()
     return 200
 
 @app.post('/add/accident/')
 async def addAccident(id: int, carid: int, damage: str):
-    cur.execute(f'INSERT INTO vr.accident (ID, damaged, accidentDate) VALUES ({id}, {damage}, \'{str(datetime.date.today())}\');')
-    cur.execute(f'insert into vr.a2c (carid, accidentid) values ({carid}, {id});')
+    cur.execute(f'INSERT INTO vasyutinsky_ryabov.accident (ID, damaged, accidentDate) VALUES ({id}, {damage}, \'{str(datetime.date.today())}\');')
+    cur.execute(f'insert into vasyutinsky_ryabov.a2c (carid, accidentid) values ({carid}, {id});')
     conn.commit()
     return 200
 
@@ -86,12 +86,12 @@ async def addAccident(id: int, carid: int, damage: str):
 
 @app.put('/upd/car/color/')
 async def updateColor(id: int, clr: str):
-    cur.execute(f'UPDATE vr.cars SET color = {clr} WHERE carID = {id};')
+    cur.execute(f'UPDATE vasyutinsky_ryabov.cars SET color = {clr} WHERE carID = {id};')
     conn.commit()
     return 200
 @app.put('/upd/car/age/')
 async def updateAge(id: int, newAge: int):
-    cur.execute(f' UPDATE vr.cars SET age = {newAge} WHERE carID = {id};')
+    cur.execute(f' UPDATE vasyutinsky_ryabov.cars SET age = {newAge} WHERE carID = {id};')
     conn.commit()
     return 200
 
@@ -99,36 +99,36 @@ async def updateAge(id: int, newAge: int):
 
 @app.delete('/rem/car/byID/')
 async def deleteCar(id: int):
-    cur.execute(f'delete from vr.a2c where {id} = carid;')
-    cur.execute(f'DELETE FROM vr.cars WHERE carID = {id};')
+    cur.execute(f'delete from vasyutinsky_ryabov.a2c where {id} = carid;')
+    cur.execute(f'DELETE FROM vasyutinsky_ryabov.cars WHERE carID = {id};')
     conn.commit()
     return 200
 
 @app.delete('/rem/car/byAge/')
 async def deleteCarByAge(minimum: int, maximum: int):
-    cur.execute(f'delete from vr.a2c where (select cars.carID where cars.age between {minimum} and {maximum}) = carid;')
-    cur.execute(f'DELETE FROM vr.cars WHERE age BETWEEN {minimum} AND {maximum};')
+    cur.execute(f'delete from vasyutinsky_ryabov.a2c where (select cars.carID where cars.age between {minimum} and {maximum}) = carid;')
+    cur.execute(f'DELETE FROM vasyutinsky_ryabov.cars WHERE age BETWEEN {minimum} AND {maximum};')
     conn.commit()
     return 200
 
 @app.delete('/rem/car/byType/')
 async def deleteCarByType(typ: str):
-    cur.execute(f'delete from vr.a2c where (select cars.carID where cars.carType = {typ}) = carid;')
-    cur.execute(f'DELETE FROM vr.cars WHERE carType = {typ};')
+    cur.execute(f'delete from vasyutinsky_ryabov.a2c where (select cars.carID where cars.carType = {typ}) = carid;')
+    cur.execute(f'DELETE FROM vasyutinsky_ryabov.cars WHERE carType = {typ};')
     conn.commit()
     return 200
 
 @app.delete('/rem/car/byModel/')
 async def deleteCarByModel(mdl: str):
-    cur.execute(f'delete from vr.a2c where (select cars.carID where cars.model = {mdl}) = carid;')
-    cur.execute(f'DELETE FROM vr.cars WHERE model = {mdl};')
+    cur.execute(f'delete from vasyutinsky_ryabov.a2c where (select cars.carID where cars.model = {mdl}) = carid;')
+    cur.execute(f'DELETE FROM vasyutinsky_ryabov.cars WHERE model = {mdl};')
     conn.commit()
     return 200
 
 @app.delete('/rem/accident/')
 async def deleteAccident(id: int):
-    cur.execute(f'delete from vr.a2c where {id} = accidentid;')
-    cur.execute(f'DELETE FROM vr.accident WHERE ID = {id};')
+    cur.execute(f'delete from vasyutinsky_ryabov.a2c where {id} = accidentid;')
+    cur.execute(f'DELETE FROM vasyutinsky_ryabov.accident WHERE ID = {id};')
     conn.commit()
     return 200
 
--- migrate.sql
+++ migrate.sql
@@ -1,6 +1,6 @@
-create schema if not exists vr;
+create schema if not exists vasyutinsky_ryabov;
 
-CREATE TABLE IF NOT EXISTS vr.cars (
+CREATE TABLE IF NOT EXISTS vasyutinsky_ryabov.cars (
   carID INT PRIMARY KEY,
   age INT NOT NULL,
   model TEXT NOT NULL,
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS vr.cars (
   carType TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS vr.accident (
+CREATE TABLE IF NOT EXISTS vasyutinsky_ryabov.accident (
   ID INT PRIMARY KEY,
   damaged TEXT NOT NULL,
   accidentDate DATE NOT NULL
 );
 
 
-create table if not exists vr.a2c(
+create table if not exists vasyutinsky_ryabov.a2c(
   carid int references vr.cars (carID),
   accidentid int references vr.accident (ID)
 );
