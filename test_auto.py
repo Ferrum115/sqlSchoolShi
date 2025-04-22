7f316591aa8b58e92da3f2e3202e3572c158ca02
Ferrum115 <neomozg15@gmail.com>
2025-04-22 17:38:56 +0500

    idk how will you launch pytest on your own PC

--- test_auto.py
+++ test_auto.py
@@ -6,12 +6,12 @@ from main import app
 
 client = TestClient(app)
 
-def read_add_func_test():
+def test_add_func():
     response = client.post('/add/cars/byID/?id=10&ag=10&mdl=sport&clr=blu&typ=yay')
     assert response.status_code == 200
 
 
-def read_get_func_test():
+def test_get_func():
     response = client.get("/get/cars/byID/?id=10")
     assert response.status_code == 200
     assert response.json() == {"carID": "10",
@@ -20,11 +20,11 @@ def read_get_func_test():
                                "color": "blu",
                                "type": "yay"}
 
-def read_put_func_test():
+def test_put_func():
     response = client.put("/upd/car/color/?id=10&clr=red")
     assert response.status_code == 200
 
-def read_del_func_test():
+def test_del_func():
     response = client.delete("/rem/car/byID/?id=10")
     assert response.status_code == 200
 
