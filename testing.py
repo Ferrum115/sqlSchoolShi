0d1b7955ebc2976b634aac5038898bb214133263
Ferrum115 <neomozg15@gmail.com>
2025-04-22 17:20:25 +0500

    -

--- testing.py
+++ testing.py
@@ -28,25 +28,3 @@ def read_del_func_test():
     response = client.delete("/rem/car/byID/")
     assert response.status_code == 200
 
-# def read_add_func_test():
-#     r = addCar(id=10, ag=10, mdl="sport", clr="blu", typ="yay")
-#     if r['carID'] != '10':
-#         print("car assignment failed")
-#     if not r['carID']:
-#         print("car assignment failed")
-#     if r['age'] != '10':
-#         print("car assignment failed")
-#     if not r['age']:
-#         print("car assignment failed")
-#     if r['model'] != 'soprt':
-#         print("car assignment failed")
-#     if not r['model']:
-#         print("car assignment failed")
-#     if r['color'] != 'blu':
-#         print("car assignment failed")
-#     if not r['color']:
-#         print("car assignment failed")
-#     if r['carType'] != 'yay':
-#         print("car assignment failed")
-#     if not r['carType']:
-#         print("car assignment failed")
